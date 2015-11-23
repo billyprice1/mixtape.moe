3f1aeb2aac895e725180288a48efc2204ca8cddc
Drybones <drybones@drybones.me>
2015-11-22 20:53:54 -0500

    minor style changes to content and faq updated

--- faq/index.php
+++ faq/index.php
@@ -61,18 +61,16 @@
 		<div class="content">
 			<ul>
 				<li>Mixtape.moe does not support older versions of Internet Explorer, Firefox, Chrome, Opera and Safari.</li>
-				<li>Max file size is 100MB, for now at least. .</li>
-				<li>Max file uploads at once is 50. The combined total must <= 100MB.</li>
+				<li>Max file size is 100MB, for now at least. Max file uploads at once is 50. The combined total must <= 100MB.</li>
 				<li>The whole site uses full SSL/TLS HTTPS encryption with <a href="https://en.wikipedia.org/wiki/SPDY">SPDY</a>.</li>
-				<li>No Child porn. Ever. No viruses or malware uploads.</li>
-				<li>Just about all filetypes are allowed, ALL! (gif,jpg,cbr,rar,ass,srt,etc,etc) with the exception of .exe due to abuse usage.</li>
-				<li>Any file can be removed or replaced at any time for any reason.</li>
+				<li>No Child porn. Ever. No viruses or malware uploads. Any file that meets these criteria will be removed immediately.</li>
+				<li>Just about all filetypes are allowed, (gif,jpg,cbr,rar,ass,srt,etc,etc) with the exception of .exe due to abuse usage.</li>
 				<li>Some logs are kept temporarily. Access logs are rotated daily and destroyed after 3 days.</li>
-				<li>mixtape.moe is hosted on a 1 Gigabit Solid State Drive server in Newark, NJ, US and Cloudflare's CDN helps out, I'm on a restrained budget.</li>
-				<li>I want to deliver the best free service I can without ads so donations are very welcomed via the donate button on the front page and below via the Donate button.</li>
-				<li>Malware scans are consistently done. Viruses will be quarantined if detected, we want to avoid my.mixtape.moe ending up on blacklists.</li>
+				<li>mixtape.moe is hosted on a 1 Gigabit SSD server in New York, US and Cloudflare's CDN helps out.</li>
+				<li>I want to deliver the best free service I can, but services cost money. Consider donating.</li>
+				<li>Malware scans are consistently done. Malware will be quarantined if detected, we want to avoid my.mixtape.moe ending up on blacklists.</li>
 				<li>If you believe your file was wrongly removed as a false virus detection, contact us at <a href="mailto:admin@mixtape.moe">admin@mixtape.moe</a></li>
-				<li>For removal of a file, contact us at <a href="mailto:abuse@mixtape.moe">abuse@mixtape.moe</a> for everything else, contact <a href="mailto:admin@mixtape.moe">admin@mixtape.moe</a></li>		 
+				<li>For removal of a file, contact us at <a href="mailto:abuse@mixtape.moe">abuse@mixtape.moe</a> for everything else, use the admin email.</li>		 
 				<li>Mixtape.moe opened up on July 9th, 2015. I hope you rike it.</li>
 			</ul>
 			<p><b>The FAQ may also be changed and updated at any time.<br>Have other questions? Ask me shit on <a href="https://twitter.com/drybones5/">Twitter</a>.</b></p>
