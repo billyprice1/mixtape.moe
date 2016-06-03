e0c41ebf21c18a94557c9f0d02f5f729d768a90c
Drybones <drybones@drybones.me>
2016-06-03 15:17:03 -0400

    heh

--- index.php
+++ index.php
@@ -15,63 +15,65 @@
 				<?php include('global/alerts.html'); ?> 
 		</div>
 	</div>
-	<div class="container">
-		<div class="header">
-			<div class="logo">
-				<a href="https://mixtape.moe/">
-					<div class="logo-container">
-					  <div class="tape">
-					    <div class="label">
-					      <div class="labelbg">
-					        <div class="line1"></div><div class="line2"></div>
-					        <h4 class="label-logo">mixtape.moe</h4>
-					      </div>
-					      <div class="reels">
-					        <div class="leftreel">
-					        </div>
-					        <div class="window">
-					          <div class="leftwinreel">
-					          </div>
-					          <div class="rightwinreel">
-					          </div>
-					        </div>
-					        <div class="rightreel">
-					        </div>
-					      </div>
-					    </div>
-					    <div class="bevel">
-					      
-					    </div>
-					    <div class="lefthole">
-					      </div>
-					      <div class="leftinner">
-					      </div>
-					      <div class="rightinner">
-					      </div>
-					      <div class="righthole">
-					      </div>
-					  </div>
-					</div>
-				</a>
+	<div class="wrapper">
+		<div class="container">
+			<div class="header">
+				<div class="logo">
+					<a href="https://mixtape.moe/">
+						<div class="logo-container">
+						  <div class="tape">
+						    <div class="label">
+						      <div class="labelbg">
+						        <div class="line1"></div><div class="line2"></div>
+						        <h4 class="label-logo">mixtape.moe</h4>
+						      </div>
+						      <div class="reels">
+						        <div class="leftreel">
+						        </div>
+						        <div class="window">
+						          <div class="leftwinreel">
+						          </div>
+						          <div class="rightwinreel">
+						          </div>
+						        </div>
+						        <div class="rightreel">
+						        </div>
+						      </div>
+						    </div>
+						    <div class="bevel">
+						      
+						    </div>
+						    <div class="lefthole">
+						      </div>
+						      <div class="leftinner">
+						      </div>
+						      <div class="rightinner">
+						      </div>
+						      <div class="righthole">
+						      </div>
+						  </div>
+						</div>
+					</a>
+				</div>
+				<div class="header-text">
+					<h1 class="sitename">Mixtape.moe</h1>
+					<h2 class="slogan">Drop Your Fire, Fam</h2>
+					<h3 class="instruct">Upload and share any file up to 100MB</h3>
+				</div>
 			</div>
-			<div class="header-text">
-				<h1 class="sitename">Mixtape.moe</h1>
-				<h2 class="slogan">Drop Your Fire, fam</h2>
-				<h3 class="instruct">Share and upload any file up to 100MB</h3>
+			<div class="jumbotron">
+				
+				<p class="stats"><?php include('global/stats.html'); ?></p>
+				<noscript><p class="alert alert-error"><strong>Enable JavaScript</strong> you fucking autist neckbeard, it's not gonna hurt you</p></noscript>
+				<p id="no-file-api" class="alert alert-error"><strong>Your browser is not fire fam.</strong> Install the latest<a href="https://firefox.com/">Firefox</a> or <a href="https://chrome.google.com/">Google Botnet</a> and come back &lt;3</p>
+				<a href="javascript:;" id="upload-btn" class="btn">Select <span>or drop your </span>file(s)</a>
+				<br>or <a href="https://mixtape.moe/mic.php">record a voice file</a>
+				<input type="file" id="upload-input" name="files[]" multiple="multiple" data-max-size="100MiB">
+				<ul id="upload-filelist"></ul>
 			</div>
-		</div>
-		<div class="jumbotron">
-			
-			<p class="stats"><?php include('global/stats.html'); ?></p>
-			<noscript><p class="alert alert-error"><strong>Enable JavaScript</strong> you fucking autist neckbeard, it's not gonna hurt you</p></noscript>
-			<p id="no-file-api" class="alert alert-error"><strong>Your browser is shit.</strong> Install the latest<a href="https://firefox.com/">Firefox</a> or <a href="https://chrome.google.com/">Google Botnet</a> and come back &lt;3</p>
-			<a href="javascript:;" id="upload-btn" class="btn">Select <span>or drop your </span>file(s)</a>
-			<br>or record a <a href="https://mixtape.moe/mic.php">voice</a> file
-			<input type="file" id="upload-input" name="files[]" multiple="multiple" data-max-size="100MiB">
-			<ul id="upload-filelist"></ul>
-		</div>
-		<div class="footer">
-			<?php include('global/footer.html'); ?> 
+			<div class="footer">
+				<?php include('global/footer.html'); ?> 
+			</div>
 		</div>
 	</div>
 </body>
