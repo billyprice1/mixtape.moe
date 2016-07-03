8548428bc9c9859733560828294d64ff348b9fef
Drybones <drybones@drybones.me>
2016-07-03 16:36:27 -0400

    AMERICA BIRTHDAY

--- index.php
+++ index.php
@@ -16,11 +16,28 @@
 	</div>
 	<div class="wrapper">
 		<div class="home-container">
+  <video autoplay loop style="
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -100;
+    -ms-transform: translateX(-50%) translateY(-50%);
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    background-size: cover;
+">
+    <source src="https://my.mixtape.moe/bkzwyg.webm" type="video/webm">
+</video>
 			<div class="header">
 				<div class="logo">
 					<a href="https://mixtape.moe/">
 						<div class="logo-container">
-						  <div class="tape">
+						  <div class="tape" style="background: #2939b1 !important;">
 						    <div class="label">
 						      <div class="labelbg">
 						        <div class="line1"></div><div class="line2"></div>
@@ -39,16 +56,15 @@
 						        </div>
 						      </div>
 						    </div>
-						    <div class="bevel">
-						      
+						    <div class="bevel" style="border-bottom: 39px solid #1f2a77;border-left: 11px solid #2939b1;border-right: 11px solid #2939b1;">
 						    </div>
-						    <div class="lefthole">
+						    <div class="lefthole" style="background-color: #ebebeb;">
 						      </div>
-						      <div class="leftinner">
+						      <div class="leftinner" style="background-color: #ebebeb;">
 						      </div>
-						      <div class="rightinner">
+						      <div class="rightinner" style="background-color: #ebebeb;">
 						      </div>
-						      <div class="righthole">
+						      <div class="righthole" style="background-color: #ebebeb;">
 						      </div>
 						  </div>
 						</div>
@@ -56,23 +72,23 @@
 				</div>
 				<div class="header-text-home">
 					<h1 class="sitename white-color">Mixtape.moe</h1>
-					<h2 class="slogan">Drop Your Fire, Fam</h2>
-					<h3 class="instruct">Upload and share any file up to 100MB</h3>
+					<h2 class="slogan white-color">HAPPY BIRTHDAY AMERICA</h2>
+					<h3 class="instruct white-color">Upload and share any file up to 100MB</h3>
 				</div>
 			</div>
 			<div class="jumbotron">
 				
-				<p class="stats"><a href="stats"><?php include('global/stats.html'); ?></a></p>
+				<p class="stats white-color"><a href="stats white-color"><?php include('global/stats.html'); ?></a></p>
 				<noscript><p class="alert alert-error"><strong>Enable JavaScript</strong> you fucking autist neckbeard, it's not gonna hurt you</p></noscript>
 				<p id="no-file-api" class="alert alert-error"><strong>Your browser is not fire fam.</strong> Install the latest<a href="https://firefox.com/">Firefox</a> or <a href="https://chrome.google.com/">Google Botnet</a> and come back &lt;3</p>
-				<a href="javascript:;" id="upload-btn" class="btn">Select <span>or drop your </span>file(s)</a>
-				<br><a href="mic.php">record from mic</a>
+				<a href="javascript:;" id="upload-btn" class="btn" style="border: 4px dashed rgba(255, 255, 255, 0.5);color: rgba(255, 255, 255, 0.5);">Select <span>or drop your </span>file(s)</a>
+				<br><a href="mic.php" class="white-color">record from mic</a>
 				<input type="file" id="upload-input" name="files[]" multiple="multiple" data-max-size="100MiB">
 				<ul id="upload-filelist"></ul>
 			</div>
 		</div>
 	</div>
-	<div class="footer">
+	<div class="footer white-color">
 		<?php include('global/footer.html'); ?> 
 	</div>
 </body>
